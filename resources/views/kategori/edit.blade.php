68290c0b5aa5904367be5256da2a1cb101697ed5
projekgit <muhammaddarajat80@gmail.com>
2024-09-25 12:43:39 +0700

    rrr

--- resources/views/kategori/edit.blade.php
+++ resources/views/kategori/edit.blade.php
@@ -4,15 +4,15 @@
 <div class="container">
     <h1>Edit Category</h1>
 
-    <form action="{{ route('kategori.edit', $kategori) }}" method="POST">
-        @csrf
-        @method('PUT')
-        <div class="mb-3">
-            <label for="nama" class="form-label">Nama Category </label>
-            <input type="text" name="nama" class="form-control" value="{{ $kategori->nama }}" required>
-        </div>
+    <form action="{{ route('kategori.update', $kategori->id) }}" method="POST">
+    @csrf
+    @method('PATCH')
+    <div class="mb-3">
+        <label for="nama" class="form-label">Nama Category </label>
+        <input type="text" name="nama" class="form-control" value="{{ $kategori->nama }}" required>
+    </div>
 
-        <button type="submit" class="btn btn-primary">Update Category</button>
-    </form>
+    <button type="submit" class="btn btn-primary">Update Category</button>
+</form>
 </div>
 @endsection
